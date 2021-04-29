6a496aecae3e8a3d53349ccff2a79146630a7220
JorgeBeck <jorge.becerra@onephase.com>
2021-04-29 13:19:02 -0500

    Add PageLayout component for pages, create Services dummy page

---
+++ pages/services.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+import PageLayout from '../components/page-layout';
+
+const Services: FC = () => {
+  return (
+    <PageLayout title="Onephase Services Component">
+      <h1>Onephase Services</h1>
+      <p>
+        I am a services page
+        <br />
+        TEXT
+      </p>
+    </PageLayout>
+  );
+};
+
+export default Services;
